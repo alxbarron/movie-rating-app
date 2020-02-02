d6ebd2892d01531ef5f4959d7c9576df167879a6
alxbastard <alxbarron@gmail.com>
2020-02-01 23:04:34 -0600

    Add content on Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,179 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+  <!-- <div class="home">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  </div> -->
+    <v-container fluid>
+      <v-row no-gutters>
+        <v-col sm="4">
+          <v-card outlined tile class="text-left">
+            <v-card-title class="headline">
+                Batman vs Superman
+            </v-card-title>
+            <v-card-subtitle class="grey--text">
+                2016 · Science fiction film/Action fiction · 3h 3m
+            </v-card-subtitle>
+            <v-card-text class="text--left">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+              ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+              in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+              officia deserunt mollit anim id est laborum.
+            </v-card-text>
+            <v-card-actions>
+              <v-btn text color="purple">Rate this movie</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-col sm="4">
+          <v-card outlined tile class="text-left">
+            <v-card-title class="headline">
+                Batman vs Superman
+            </v-card-title>
+            <v-card-subtitle class="grey--text">
+                2016 · Science fiction film/Action fiction · 3h 3m
+            </v-card-subtitle>
+            <v-card-text class="text--left">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+              ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat.
+            </v-card-text>
+            <v-card-actions>
+              <v-btn text color="purple">Rate this movie</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-col sm="4">
+          <v-card outlined tile class="text-left">
+            <v-card-title class="headline">
+                Batman vs Superman
+            </v-card-title>
+            <v-card-subtitle class="grey--text">
+                2016 · Science fiction film/Action fiction · 3h 3m
+            </v-card-subtitle>
+            <v-card-text class="text--left">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+              ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+              in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+              officia deserunt mollit anim id est laborum.
+            </v-card-text>
+            <v-card-actions>
+              <v-btn text color="purple">Rate this movie</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-col sm="4">
+          <v-card outlined tile class="text-left">
+            <v-card-title class="headline">
+                Batman vs Superman
+            </v-card-title>
+            <v-card-subtitle class="grey--text">
+                2016 · Science fiction film/Action fiction · 3h 3m
+            </v-card-subtitle>
+            <v-card-text class="text--left">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+              ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat.
+            </v-card-text>
+            <v-card-actions>
+              <v-btn text color="purple">Rate this movie</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-col sm="4">
+          <v-card outlined tile class="text-left">
+            <v-card-title class="headline">
+                Batman vs Superman
+            </v-card-title>
+            <v-card-subtitle class="grey--text">
+                2016 · Science fiction film/Action fiction · 3h 3m
+            </v-card-subtitle>
+            <v-card-text class="text--left">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+              ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+              in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+              officia deserunt mollit anim id est laborum.
+            </v-card-text>
+            <v-card-actions>
+              <v-btn text color="purple">Rate this movie</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-col sm="4">
+          <v-card outlined tile class="text-left">
+            <v-card-title class="headline">
+                Batman vs Superman
+            </v-card-title>
+            <v-card-subtitle class="grey--text">
+                2016 · Science fiction film/Action fiction · 3h 3m
+            </v-card-subtitle>
+            <v-card-text class="text--left">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+              ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+              in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+              officia deserunt mollit anim id est laborum.
+            </v-card-text>
+            <v-card-actions>
+              <v-btn text color="purple">Rate this movie</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-col sm="4">
+          <v-card outlined tile class="text-left">
+            <v-card-title class="headline">
+                Batman vs Superman
+            </v-card-title>
+            <v-card-subtitle class="grey--text">
+                2016 · Science fiction film/Action fiction · 3h 3m
+            </v-card-subtitle>
+            <v-card-text class="text--left">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+              ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+              in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+              officia deserunt mollit anim id est laborum.
+            </v-card-text>
+            <v-card-actions>
+              <v-btn text color="purple">Rate this movie</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+// import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld,
+  data: () => ({
+    message: 'Hello there, how are you today?',
+    btnTxt: 'Reply',
+    flag: true,
+  }),
+  methods: {
+    reply() {
+      this.message = this.flag ? 'I\'m doing great. Thank You!' : 'Hello there, how are you today?';
+      this.btnTxt = this.flag ? 'Greet' : 'Reply';
+      this.flag = !this.flag;
+    },
   },
+  components: { },
 };
 </script>
