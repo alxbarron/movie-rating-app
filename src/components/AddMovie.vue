8f4477e53425c721b82c6f584b95a4aa5dcce389
alxbastard <alxbarron@gmail.com>
2020-02-03 00:39:38 -0600

    :sparkles: Backend server & Movies CRUD setup with create EndPoint

--- src/components/AddMovie.vue
+++ src/components/AddMovie.vue
@@ -29,6 +29,8 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data: () => ({
     valid: true,
@@ -51,8 +53,24 @@ export default {
   methods: {
     submit() {
       if (this.$refs.form.validate()) {
-        // Perform next action
+        return axios({
+          method: 'post',
+          data: {
+            name: this.name,
+            description: this.description,
+            release_year: this.release_year,
+            genre: this.genre,
+          },
+          url: 'http://localhost:8081/movies',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }).then(() => {
+          this.$router.push({ name: 'Home' });
+          this.$refs.form.reset();
+        }).catch(() => {});
       }
+      return true;
     },
     clear() {
       this.$refs.form.reset();
